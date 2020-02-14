2a3d5fc882c911c3ba7a9c44dd33d452c7887ec6
Elena <Elena-Bonito@yandex.ru>
2020-02-14 12:48:22 +0100

    добавила пропсы у MoviePage

--- src/components/movie-page/movie-page.jsx
+++ src/components/movie-page/movie-page.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+const bigMovieCard = {
+  movieTitle: `The Grand Budapest Hotel`,
+  movieGenre: `Drama`,
+  movieYear: 2014,
+  moviePoster: `img/the-grand-budapest-hotel-poster.jpg`,
+  movieBG: `img/bg-the-grand-budapest-hotel.jpg`,
+  overView: {
+    movieRatingScore: `8,9`,
+    movieRatingLevel: `Very good`,
+    movieRatingCount: `240 ratings`,
+    movieDescription: ``,
+    movieDirector: `Wes Andreson`,
+    movieStarring: `Bill Murray, Edward Norton, Jude Law, Willem Dafoe and other`,
+  },
 
-const MoviePage = () => {
+};
+
+const MoviePage = (props) => {
+  const {bigMovieCard} = props;
+  const {movieTitle, movieGenre, movieYear, moviePoster, movieBG, overView} = bigMovieCard;
+  const {movieRatingScore, movieRatingLevel, movieRatingCount, movieDirector, movieStarring} = overView;
   return <section className="movie-card movie-card--full">
     <div className="movie-card__hero">
       <div className="movie-card__bg">
-        <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel"/>
+        <img src={movieBG} alt={movieTitle}/>
       </div>
 
       <h1 className="visually-hidden">WTW</h1>
@@ -28,10 +47,10 @@ const MoviePage = () => {
 
       <div className="movie-card__wrap">
         <div className="movie-card__desc">
-          <h2 className="movie-card__title">The Grand Budapest Hotel</h2>
+          <h2 className="movie-card__title">{movieTitle}</h2>
           <p className="movie-card__meta">
-            <span className="movie-card__genre">Drama</span>
-            <span className="movie-card__year">2014</span>
+            <span className="movie-card__genre">{movieGenre}</span>
+            <span className="movie-card__year">{movieYear}</span>
           </p>
 
           <div className="movie-card__buttons">
@@ -56,7 +75,7 @@ const MoviePage = () => {
     <div className="movie-card__wrap movie-card__translate-top">
       <div className="movie-card__info">
         <div className="movie-card__poster movie-card__poster--big">
-          <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327"/>
+          <img src={moviePoster} alt={movieTitle} width="218" height="327"/>
         </div>
 
         <div className="movie-card__desc">
@@ -75,10 +94,10 @@ const MoviePage = () => {
           </nav>
 
           <div className="movie-rating">
-            <div className="movie-rating__score">8,9</div>
+            <div className="movie-rating__score">{movieRatingScore}</div>
             <p className="movie-rating__meta">
-              <span className="movie-rating__level">Very good</span>
-              <span className="movie-rating__count">240 ratings</span>
+              <span className="movie-rating__level">{movieRatingLevel}</span>
+              <span className="movie-rating__count">{movieRatingCount}</span>
             </p>
           </div>
 
@@ -90,15 +109,32 @@ const MoviePage = () => {
               sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously,
               Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.</p>
 
-            <p className="movie-card__director"><strong>Director: Wes Andreson</strong></p>
+            <p className="movie-card__director"><strong>Director: {movieDirector}</strong></p>
 
-            <p className="movie-card__starring"><strong>Starring: Bill Murray, Edward Norton, Jude Law, Willem Dafoe and
-              other</strong></p>
+            <p className="movie-card__starring"><strong>Starring: {movieStarring}</strong></p>
           </div>
         </div>
       </div>
     </div>
   </section>;
+};
+
+MoviePage.propTypes = {
+  bigMovieCard: PropTypes.shape({
+    movieTitle: PropTypes.string.isRequired,
+    movieGenre: PropTypes.string.isRequired,
+    movieYear: PropTypes.number.isRequired,
+    moviePoster: PropTypes.string.isRequired,
+    movieBG: PropTypes.string.isRequired,
+    overView: PropTypes.shape({
+      movieRatingScore: PropTypes.string.isRequired,
+      movieRatingLevel: PropTypes.string.isRequired,
+      movieRatingCount: PropTypes.string.isRequired,
+      movieDescription: PropTypes.string,
+      movieDirector: PropTypes.string.isRequired,
+      movieStarring: PropTypes.string.isRequired,
+    }).isRequired
+  }).isRequired,
 };
 
 export default MoviePage;
