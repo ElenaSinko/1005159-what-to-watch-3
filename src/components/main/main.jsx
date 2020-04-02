35cf276ce038afebbe1e2adccb8bc2017405e57a
Elena <Elena-Bonito@yandex.ru>
2020-04-02 11:49:35 +0100

    исправила цвет кнопки Play

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -84,7 +84,7 @@ class Main extends PureComponent {
 
               <div className="movie-card__buttons">
                 <button className="btn btn--play movie-card__button" type="button">
-                  <Link to={`${PAGES.PLAYER}/${promoFilm.id}`} style={{textDecoration: `none`}}>
+                  <Link to={`${PAGES.PLAYER}/${promoFilm.id}`} style={{textDecoration: `none`, color: `#eee5b5`}}>
                     <svg viewBox="0 0 19 19" width="19" height="19">
                       <use xlinkHref="#play-s"></use>
                     </svg>
--- src/components/movie-page/movie-page.jsx
+++ src/components/movie-page/movie-page.jsx
@@ -92,7 +92,7 @@ class MoviePage extends PureComponent {
 
             <div className="movie-card__buttons">
               <button className="btn btn--play movie-card__button" type="button">
-                <Link to={`${PAGES.PLAYER}/${movieCard.id}`} style={{textDecoration: `none`}}>
+                <Link to={`${PAGES.PLAYER}/${movieCard.id}`} style={{textDecoration: `none`, color: `#eee5b5`}}>
                   <svg viewBox="0 0 19 19" width="19" height="19">
                     <use xlinkHref="#play-s"></use>
                   </svg>
